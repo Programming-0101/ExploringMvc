72d9bc1e4fcdc6c615a62dd8dc8d797d41355ae8
dagilleland <dagilleland@shaw.ca>
2017-10-24 08:00:56 -0600

    Begin MVC Project
    A simple MVC project with no authentication.

---
+++ ExploringMvc/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExploringMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "How Model-View-Controller Websites Work in ASP.Net.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
